8ec8b653840a7ad764a0d0990131fa78b4fcf3fe
sosv-hydro <sofia.saavedra1@upr.edu>
2019-02-01 23:03:45 -0400

    seed

---
+++ home.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/login")
+def login():
+    return render_template('login.html', title='Log in')
+
+@app.route("/register")
+def register():
+    return render_template('register.html', title='Log in')
+
+if __name__ == '__main__':
+    app.run(debug=True)
